5fbcb52e7bab777ae8f8a01713b3038177129469
unknown <georgecuray7@gmail.com>
2022-11-04 11:53:29 +0800

    added dashboard for the user

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 import router from "./router";
-import App from './App.vue'
+import App from "./App.vue";
 
+const pinia = createPinia();
+const app = createApp(App);
 
-const pinia = createPinia()
-const app = createApp(App)
-
-app.use(pinia)
-app.use(router)
-app.mount('#appId')
+app.use(pinia);
+app.use(router);
+app.mount("#appId");
